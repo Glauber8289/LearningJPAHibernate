4864d224ea06b4a7c7097862c305b0ee5b34394f
Glauber8289 <Petersonglauber@hotmail.com>
2023-08-07 22:37:42 -0300

    Update Program

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -6,13 +6,12 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1= new Pessoa (1," Carlos jose","Carlosjose@gmail.com");
-		Pessoa p2= new Pessoa(2, "Joaquim torres", "JoaquimTorres@hotmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "Ana@gmail.com");
+		Pessoa p1= new Pessoa (null," Carlos jose","Carlosjose@gmail.com");
+		Pessoa p2= new Pessoa(null, "Joaquim torres", "JoaquimTorres@hotmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "Ana@gmail.com");
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+	 
+		
 		
 	}
 
