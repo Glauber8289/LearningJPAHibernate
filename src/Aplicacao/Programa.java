72b3adfd78ba81162ca7aa80b603075961c38fb8
Glauber8289 <Petersonglauber@hotmail.com>
2023-08-08 23:58:45 -0300

    Update Program and Transaction

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,5 +1,9 @@
 package Aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import Dominio.Pessoa;
 
 public class Programa {
@@ -11,7 +15,18 @@ public class Programa {
 		Pessoa p3 = new Pessoa(null, "Ana Maria", "Ana@gmail.com");
 
 	 
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
 		
+		em.getTransaction().begin();
+		
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+	
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 		
 	}
 
