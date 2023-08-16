27a112570474b71b9e2c32aa0571b2e28b2aa20a
Glauber8289 <Petersonglauber@hotmail.com>
2023-08-15 23:53:02 -0300

    Buscando dados no Banco de dados

--- src/main/java/Aplicacao/Programa.java
+++ src/main/java/Aplicacao/Programa.java
@@ -10,24 +10,21 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1= new Pessoa (null," Carlos jose","Carlosjose@gmail.com");
-		Pessoa p2= new Pessoa(null, "Joaquim torres", "JoaquimTorres@hotmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "Ana@gmail.com");
-
+		
 	 
 		EntityManagerFactory emf= Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
 		em.getTransaction().begin();
 		
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
 		
-	
-		em.getTransaction().commit();
+	     Pessoa p=em.find(Pessoa.class, 2);
+	     
+	     System.out.println(p);
+		
 		System.out.println("Pronto");
-		
+		em.close();
+		emf.close();
 	}
 
 }
